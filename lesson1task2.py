57e731ae89f1c4f288e7c2212ad24f97b4752678
DimMilnov <a_aler@mail.ru>
2021-01-21 14:09:36 +0300

    The structure of programs has been created, task 1

---
+++ lesson1task2.py
@@ -0,0 +1,7 @@
+# Урок №1 - Задание №1
+# (Lesson №1 - Task №1)
+# Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+# (Work with variables, create several, display,
+# ask the user for several numbers and strings and save them to variables, display.)
+
